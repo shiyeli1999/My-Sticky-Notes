b1978044a6d6c55ecbeedd74f3e675c5e0c6b9a1
Doris Li <shiyeli@Doriss-MacBook-Pro-2.local>
2020-01-11 01:27:45 -0500

    Updates

---
+++ precache-manifest.2f8780f20ff0b6c0a577666f683e315b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124e85492720b931bd0f87432a67169b",
+    "url": "/My-Sticky-Notes/index.html"
+  },
+  {
+    "revision": "a3208d8e65a3b5dce74a",
+    "url": "/My-Sticky-Notes/static/js/2.f803fc08.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/My-Sticky-Notes/static/js/2.f803fc08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10c1df1a14ce23b2b830",
+    "url": "/My-Sticky-Notes/static/js/main.62f85798.chunk.js"
+  },
+  {
+    "revision": "f26bb9a622f91615c8b4",
+    "url": "/My-Sticky-Notes/static/js/runtime-main.eb59eb70.js"
+  }
+]);
